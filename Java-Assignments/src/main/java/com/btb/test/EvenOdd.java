5ce9ce3fa6ea5347473354c7e0f8eda9056f491e
Bhuvan Bolem <bhuvantejb@gmail.com>
2022-07-20 14:41:05 -0500

    Edited PrimeNumbers

--- Java-Assignments/src/main/java/com/btb/test/EvenOdd.java
+++ Java-Assignments/src/main/java/com/btb/test/EvenOdd.java
@@ -1,4 +1,9 @@
 package com.btb.test;
 public class EvenOdd {
-
+    /**
+     *
+     * SAME AS QUESTION 2
+     * Look at IfOddEven.java for this solution
+     *
+     */
 }
--- Java-Assignments/src/main/java/com/btb/test/PrimeNumbers.java
+++ Java-Assignments/src/main/java/com/btb/test/PrimeNumbers.java
@@ -1,17 +1,36 @@
 package com.btb.test;
+
+/**
+ *
+ * Need to understand the proper logic!
+ *
+ */
+
 public class PrimeNumbers {
     public static void main(String[] args) {
-        int tracker;
-        for (int i = 1; i < 100; i++){
-            tracker = 0;
-            for (int j = i; j >= 1; j--){
-                if (i % j == 0){
-                    tracker++;
-                }
-            }
-            if (tracker == 2){
+//        int tracker;
+//        for (int i = 1; i < 100; i++){
+//            tracker = 0;
+//            for (int j = i; j >= 1; j--){
+//                if (i % j == 0){
+//                    tracker++;
+//                }
+//            }
+//            if (tracker == 2){
+//                System.out.print(i + " ");
+//            }
+//        }
+        //Wilson Theorem
+        int i = 2;
+        int j = 2;
+        do {
+            if (i == 2){
                 System.out.print(i + " ");
             }
-        }
+            else if((i-1) != ((i-1) % i)){
+                System.out.print(i + " ");
+            }
+            i++;
+        }while (i <= 100);
     }
 }
